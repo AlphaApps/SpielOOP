31cc702b65ba37faa9b8f2ae3f9607d8ea13e526
JasminRai <jasminsinghrai@yahoo.de>
2021-04-30 23:10:13 +0200

    Added mouse event listner, implemented interface KlickBrettSpiel

--- src/MineFrame.java
+++ src/MineFrame.java
@@ -1,5 +1,3 @@
-import java.awt.Graphics;
-
 import javax.swing.JFrame;
 
 public class MineFrame extends JFrame{
--- src/MinePanel.java
+++ src/MinePanel.java
@@ -1,39 +1,83 @@
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
 
-public class MinePanel extends JPanel 
+public class MinePanel extends JPanel implements MouseListener 
 {
 	
-	final int X = 12;
-	final int Y = 16;
-	final int MINEN = 20;
+	private int x, y;
+
+	private KlickBrettSpiel spiel = new MinesweeperSpiel();
+	 private int X = spiel.getSpaltenZahl();
+	 private int Y = spiel.getZeilenZahl(); 
+
+	private int h;
+	private boolean result;
+	private String temp; 
 	
-	private Position minen = new Position(MINEN, X, Y);
-	private Anzeige anzeige = new Anzeige(X, Y);
-	
-	private char symbol;
-	private int p;
+	public MinePanel()
+	{
+		this.addMouseListener(this);
+	}
 
 	@Override
 	public void paint(Graphics gr) 
 	{
 		super.paint(gr);
 		
-		p = getHeight()/Y;
-		
+		h = getHeight()/Y;
 		for (int i = 0; i < X; i++)
 		{
 			for (int j = 0; j < Y; j++)
 			{
-				gr.drawRect(p * i, p * j, p, p);
-				symbol = minen.suche(new Koordinaten(i,j));
-				anzeige.set(i,j, symbol);
-				gr.drawString(anzeige.get(i, j), p * i + (p/2) , p * j + (p/2));
+				gr.drawRect(h * i, h * j, h, h);
+				spiel.set(i,j);
+				temp =  spiel.get(i, j)+"";
+				gr.drawString(temp, h * i + (h/2) , h * j + (h/2));
 			}
-		}		
+		}
+		
+		gr.setColor(Color.PINK);
+		gr.fillRect(x-(h/2), y-(h/2), h, h);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent a) {
+		result = spiel.gewonnen();
+		System.out.println(result);				
+		x = a.getX();
+		y = a.getY();
+		repaint();
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
